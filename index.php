c6fb34a548b7a63135819fff21fbaf5000c0ac1e
redeluni <dmanzi83@gmail.com>
2021-02-06 11:59:58 +0100

    refact: Add a function

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 // $dir = ".git/COMMIT_EDITMSG";
 
 // $dir = ".git";
-$dir = ".git/logs/refs/heads";
+// $dir = ".git/logs/refs/heads";
+$dir = ".git/refs/heads/master";
 // $dir = "C:\xampp_8\htdocs\workspace\CHANGELOG\.git\COMMIT_EDITMSG";
 // $dir = "/path/to/your/repo/";
 
@@ -29,26 +30,65 @@ $output = array();
 chdir($dir);
 exec("git log",$output);
 $history = array();
-foreach($output as $line){
-    if(strpos($line, 'commit') === 0){
-        echo '1 <br>';
-        if(!empty($commit)){
+$commit = [
+    'hash' => '',
+    'author' => '',
+    'date' => '',
+    'message' => ''
+];
+
+
+
+function parseLog($log) {
+    $history = array();
+    foreach($log as $key => $line) {
+        if(strpos($line, 'commit') === 0 || $key + 1 == count($lines)){
+            $commit['hash'] = substr($line, strlen('commit') + 1);
+        } else if(strpos($line, 'Author') === 0){
+            $commit['author'] = substr($line, strlen('Author:') + 1);
+        } else if(strpos($line, 'Date') === 0){
+            $commit['date'] = substr($line, strlen('Date:') + 3);
+        } elseif (strpos($line, 'Merge') === 0) {
+            $commit['merge'] = substr($line, strlen('Merge:') + 1);
+            $commit['merge'] = explode(' ', $commit['merge']);
+        } else if(!empty($line)){
+            $commit['message'] = substr($line, 4);
             array_push($history, $commit);
             unset($commit);
         }
-        $commit['hash'] = substr($line, strlen('commit'));
-    } else if(strpos($line, 'Author')===0){
-        echo '2 <br>';
-	    $commit['author'] = substr($line, strlen('Author:'));
-    } else if(strpos($line, 'Date')===0){
-        echo '3 <br>';
-	    $commit['date']   = substr($line, strlen('Date:'));
-    } else{
-        echo '4 <br>';
-	    // $commit['message']  .= $line;
     }
+    return $history;
 }
 
+
+$history = parseLog($output);
+
+
+
+
+// foreach($output as $line){
+//     if(strpos($line, 'commit') === 0){
+//         echo '1 <br>';
+//         if(!empty($commit)){
+//             array_push($history, $commit);
+//             // unset($commit);
+//         }
+//         $commit['hash'] = substr($line, strlen('commit'));
+//     } else if(strpos($line, 'Author')===0){
+//         echo '2 <br>';
+// 	    $commit['author'] = substr($line, strlen('Author:'));
+//     } else if(strpos($line, 'Date')===0){
+//         echo '3 <br>';
+// 	    $commit['date']   = substr($line, strlen('Date:'));
+//     } else{
+//         echo '4 <br>';
+// 	    $commit['message']  .= $line;
+//     }
+// }
+
+echo '<pre>';
 print_r($history);
+echo '</pre>';
+// print_r($history);
 
 ?>
